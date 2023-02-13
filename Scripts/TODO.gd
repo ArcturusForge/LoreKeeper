2eeedd698526eec615b061f20d5822d855e504f6
Arcturus <arcturusforge@gmail.com>
2023-02-13 17:59:19 +1100

    Implementing Tutorial

--- Scripts/TODO.gd
+++ Scripts/TODO.gd
@@ -5,6 +5,11 @@ extends Control
 # -/ Horizontal list container
 # -/?DisabledZoom? Block mouse scroll on freeformContainer
 # -/ Convert app bg to config data
+# - Hide the tutorial style from the styles selector.
+# - Stop node deletion from resetting the whole window.
+# - Fix image bug where .PNG isn't recognised as .png
+# - Rename window will show existing title in it.
+# - Add a way to shuffle the order of entries.
 # - Add options shortcuts
 # - Add active filename to top bar
 # - Add bg image selector
@@ -12,3 +17,6 @@ extends Control
 # - Create window files for the remaining categories
 # - Dynamically add options to entity popup menu
 # - Update the defaults
+# - Duplicate entity option
+# - Session file editor for restoring broken files or converting to different styles
+# - Style creator window
